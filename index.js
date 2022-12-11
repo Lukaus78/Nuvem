0fdbecd6e09e181dbe182105686deeaf27bdb800
Lukaus78 <114032337+Lukaus78@users.noreply.github.com>
2022-12-11 18:23:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,71 @@ server.use(express.static('public'));
 
 server.listen(process.env.PORT || 8888, () => "Foi!");
 
-let carrinho = []
-
 server.get("/", (req, res) => {
-    
-    return res.redirect("/produtos")
+    res.redirect("/login")
+})
+server.get("/login", (req, res) => {
+    html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercicio-2</title>
+    </head>
+    <style>
+        .input-form{
+            display: flex;
+            flex-direction: column;
+            width: 30%;
+        }
+    </style>
+    <body>
+        <div class="form">
+        <h1>Formulário</h1>
+        <div class="input-form">
+        <label>Email</label>
+        <input type="email" name="email"><br>
+        <label>Nome</label>
+        <input type="text" name="nome"><br>
+        <label>Sobrenome</label>
+        <input type="text" name="sobrenome"><br>
+        <label>Senha</label>
+        <input type="password" name="senha"><br>
+        </div>
+        <form method="post" action="/login">
+        <button style="margin-top:5px">Enviar</button>
+        </form>
+        </div>
+    </body>
+    </html>`
+
+    return res.send(html)
 
     });
 
-server.get("/produtos", (req, res) => {
+server.post("/login", urlencodedParser, (req, res) => {
+    html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Parabens</title>
+    </head>
+    <body>
+        <h1>Parabens! Você cadastrou! </h1>
+    </body>
+
+    <form method="post" action="/produtos">
+    <button>Ver produtos</button>
+    </form>
+    </html>`
+
+    return res.send(html)
+})
+
+server.post("/produtos", (req, res) => {
     
     const html1 = `
     <table style="border: 1px solid black">
@@ -66,9 +122,10 @@ server.post("/sucoDeMorango", urlencodedParser, (req, res) => {
     const html = `
     <img style="max-width:150px" src="https://atelieoficial.com.br/painel/upload/produto/suco_laranja_morango.png" alt="Suco de morango">
 
-    <form method="post" action="/add/${produto.id}">
-    <input type="hidden" value="${produto.id}"/>
+    <form method="post" action="/add/${produto}">
+    <input type="hidden" value="${produto}"/>
     <button>Adicionar ao carrinho</button>
+    </form>
     `
     return res.send(html)
 })
@@ -82,9 +139,10 @@ server.post("/coxinha", urlencodedParser, (req, res) => {
     }
     const html = `
     <img style="max-width:150px;margin-left:auto" src="https://panattos.com.br/uploads/produtos/2017/03/coxinha-de-frango-com-requeijao-mini-congelada.jpg" alt="Coxinha">
-    <form method="post" action="/add/${produto.id}">
-    <input type="hidden" value="${produto.id}"/>
-    <button>Adicionar ao carrinho</button>`
+    <form method="post" action="/add/${produto}">
+    <input type="hidden" value="${produto}"/>
+    <button>Adicionar ao carrinho</button>
+    </form>`
 
 
     return res.send(html)
@@ -99,9 +157,10 @@ server.post("/enroladinho", urlencodedParser, (req, res) => {
     }
     const html = `
     <img style="max-width:150px;margin-left:auto" src="https://marolacomcarambola.com.br/wp-content/uploads/2019/06/receita-de-enroladinho-de-salsicha-11.jpg" alt="Enroladinho">
-    <form method="post" action="/add/${produto.id}">
-    <input type="hidden" value="${produto.id}"/>
-    <button>Adicionar ao carrinho</button>`
+    <form method="post" action="/add/${produto}">
+    <input type="hidden" value="${produto}"/>
+    <button>Adicionar ao carrinho</button>
+    </form>`
 
     return res.send(html)
 
@@ -117,9 +176,10 @@ server.post("/sucoDeAbacaxi", urlencodedParser, (req, res) => {
 
     const html = `
     <img style="max-width:150px;margin-left:auto" src="https://cidadeolimpica.com.br/wp-content/uploads/2022/10/receita-de-suco-de-abacaxi-696x464.jpg" alt="Suco de abacaxi">
-    <form method="post" action="/add/${produto.id}">
-    <input type="hidden" value="${produto.id}"/>
+    <form method="post" action="/add/${produto}">
+    <input type="hidden" value="${produto}"/>
     <button>Adicionar ao carrinho</button>
+    </form>
     
     `
 
@@ -127,9 +187,3 @@ server.post("/sucoDeAbacaxi", urlencodedParser, (req, res) => {
 
 })
 
-server.post("/add/:id", urlencodedParser, (req,res) => {
-    const { id } = req.params;
-    if(id == 1 ) {
-        carrinho.push()
-    }
-})
